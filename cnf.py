100f0ca827e64cf8dd1bcb6bc223e16856494b40
tauomicronmu <tauomicronmurs@gmail.com>
2017-02-25 16:15:31 +0000

    changed how negation is handled

--- cnf.py
+++ cnf.py
@@ -1,4 +1,48 @@
-# Takes a list of 
+'''
+FOLLOWS THE FOLLOWING PSEUDOCODE: 
+
+CONVERT(φ):   // returns a CNF formula equivalent to φ
+
+// Any syntactically valid propositional formula φ must fall into
+// exactly one of the following 7 cases (that is, it is an instanceof
+// one of the 7 subclasses of Formula).
+
+If φ is a variable, then:
+   return φ.
+   // this is a CNF formula consisting of 1 clause that contains 1 literal
+
+If φ has the form P ^ Q, then:
+   CONVERT(P) must have the form P1 ^ P2 ^ ... ^ Pm, and
+   CONVERT(Q) must have the form Q1 ^ Q2 ^ ... ^ Qn,
+   where all the Pi and Qi are disjunctions of literals.
+   So return P1 ^ P2 ^ ... ^ Pm ^ Q1 ^ Q2 ^ ... ^ Qn.
+
+If φ has the form P v Q, then:
+   CONVERT(P) must have the form P1 ^ P2 ^ ... ^ Pm, and
+   CONVERT(Q) must have the form Q1 ^ Q2 ^ ... ^ Qn,
+   where all the Pi and Qi are dijunctions of literals.
+   So we need a CNF formula equivalent to
+      (P1 ^ P2 ^ ... ^ Pm) v (Q1 ^ Q2 ^ ... ^ Qn).
+   So return (P1 v Q1) ^ (P1 v Q2) ^ ... ^ (P1 v Qn)
+           ^ (P2 v Q1) ^ (P2 v Q2) ^ ... ^ (P2 v Qn)
+             ...
+           ^ (Pm v Q1) ^ (Pm v Q2) ^ ... ^ (Pm v Qn)
+
+If φ has the form ~(...), then:
+  If φ has the form ~A for some variable A, then return φ.
+  If φ has the form ~(~P), then return CONVERT(P).           // double negation
+  If φ has the form ~(P ^ Q), then return CONVERT(~P v ~Q).  // de Morgan's Law
+  If φ has the form ~(P v Q), then return CONVERT(~P ^ ~Q).  // de Morgan's Law
+
+If φ has the form P -> Q, then:
+  Return CONVERT(~P v Q).   // equivalent
+
+If φ has the form P <-> Q, then:
+  Return CONVERT((P ^ Q) v (~P ^ ~Q)).
+
+If φ has the form P xor Q, then:
+  Return CONVERT((P ^ ~Q) v (~P ^ Q)).
+
+'''
 def cnf(exp):
-    return parse(exp)
-    
+
--- parser.py
+++ parser.py
@@ -1,3 +1,4 @@
+UN_OPS = ["-"]
 BIN_OPS = ["|", "&", ":", "="]
 
 """ 
@@ -26,13 +27,27 @@ def strip_parens(exp):
     if exp.startswith('(') and exp.endswith(')') :
         return exp[1:-1]
     return exp
-   
+
+def strip_neg(exp):
+    if exp[0:2] == '-(' and exp.endswith(')') : 
+        return exp[2:-1]
+    return exp   
+
+"""
+    Returns whether or not an expression is in the form -(...)
+    (i.e. the expression is negated)
+"""
+def is_neg(exp):
+    return exp[0:2] == '-(' and exp.endswith(')')
+
 """
     Parses a propositional logic expression into a parse tree in the form
+    UnOp    ::= -
     BinOp   ::= | | & | : | =
     Literal ::= [A-Z]+[0-9]* | -[A-Z]+[0-9]*
     Tree    ::= Literal
-    Tree    ::= (Tree, BinOp, Tree) 
+    Tree    ::= (UnOp, Tree)
+    Tree    ::= (BinOp, Tree, Tree) 
 """
 def parse(exp):
     # 1. Remove all spaces from the string
@@ -43,15 +58,20 @@ def parse(exp):
 
     # 3. Split on all operators, starting at the shallowest
     def tree(exp):
+        if(is_neg(exp)) :
+            return ('-', tree(strip_neg(exp)))
         dps = depths(exp)
         max_d = max(dps)
         for i in range(max_d + 1): # For each depth, starting with the lowest...
             for j in range(len(dps) - 1): # For each char in exp
                 if dps[j] == i and exp[j] in BIN_OPS:
-                    return (tree(strip_parens(exp[0:j])), exp[j], tree(strip_parens(exp[j+1:])))
+                    return (exp[j], tree(strip_parens(exp[0:j])), tree(strip_parens(exp[j+1:])))
         return exp
 
     return tree(exp_c)
 
+
+print(parse('(-(A -> B) & (B -> (C -> D))) <-> (E | F))'))
+
 # Make sure that the parser is working as it should
-assert(parse(('((A -> B) & (B -> (C -> D))) <-> (E | F)')) == ((('A', ':', 'B'), '&', ('B', ':', ('C', ':', 'D'))), '=', ('E', '|', 'F')))
+assert(parse('(-(A -> B) & (B -> (C -> D))) <-> (E | F))') == ('=', ('-', (':', 'A', (':', 'B)&(B', (':', 'C', 'D')))), ('|', 'E', 'F)')))
