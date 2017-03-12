54043d518ffd9cf26712ae00c8f7685047c7c921
tauomicronmu <tauomicronmurs@gmail.com>
2017-03-12 13:39:09 +0000

    fix neg bug in rimp

--- cnf.py
+++ cnf.py
@@ -14,9 +14,27 @@ def rimp(tree):
     if tree[0] == '=': # Equivalence
         return ('|', ('&', rimp(tree[1]), rimp(tree[2])), 
                      ('&', ('-', rimp(tree[1])), ('-', rimp(tree[2]))))
+    if tree[0] == '-':
+        return (tree[0], rimp(tree[1])) # Negation
     return (tree[0], rimp(tree[1]), rimp(tree[2])) # Anything else
 
-
+def rneg(tree):
+    print(tree)
+    if(len(tree) == 1): # Literal
+        return tree
+    root = tree[0]
+    left = tree[1] 
+    if root == '-': # Negation
+        op = tree[1][0]
+        if(len(left) == 1):
+            return tree
+        left = tree[1][1]
+        if op == '-': # Double Negation
+            return rneg(left)
+        right = tree[1][2]
+        if op == '&' or op == '|': # DeMorgan's
+            return (op, rneg(('-', left)), rneg(('-', right)))
+    return (root, rneg(tree[1]), rneg(tree[2])) 
 
 def cnf(tree):
     return dist(rneg(rimp(tree)))
