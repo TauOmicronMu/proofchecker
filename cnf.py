4415252f7a6484302a4b4afb0f745fbdd578d9cf
tauomicronmu <tauomicronmurs@gmail.com>
2017-03-12 17:31:35 +0000

    started work on diff

--- cnf.py
+++ cnf.py
@@ -24,10 +24,10 @@ def rneg(tree):
     root = tree[0]
     left = tree[1] 
     if root == '-': # Negation
-        op = tree[1][0]
+        op = left[0]
         if(len(left) == 1):
             return tree
-        left = tree[1][1]
+        left = left[1]
         if op == '-': # Double Negation
             return rneg(left)
         right = tree[1][2]
@@ -36,5 +36,40 @@ def rneg(tree):
             return (op, rneg(('-', left)), rneg(('-', right)))
     return (root, rneg(tree[1]), rneg(tree[2])) 
 
+def dist(tree):
+    if len(tree) == 1: # Literal
+        return tree
+    root = tree[0]
+    if root == '&':
+        left = tree[1]
+        right = tree[2]
+        leftop = left[0]
+        rightop = right[0]
+        # P & (Q V R) => (P & Q) V (P & R)
+        if rightop == '|': 
+            return ('|', ('&', left, right[1]), ('&', left, right[2]))
+        # (P V R) & Q => (P & Q) V (Q & R)
+        if leftop == '|':
+            return ('|', ('&', left[1], right), ('&', left[2], right))
+    if root == '|':
+        left = tree[1]
+        right = tree[2]
+        leftop = left[0]
+        rightop = right[0]
+        # P V (Q & R) => (P V Q) & (P V R)
+        if rightop == '&':
+            return ('&', ('|', left, right[1]), ('|', left, right[2]))
+        # (P & Q) V R => (P V R) & (Q V R)
+        if leftop == '&':
+            return ('|', ('&', left[1], right), ('&', left[2], right))
+    if root == '-':
+        return (root, dist(tree[1]))
+    return (root, dist(tree[1]), dist(tree[2]))
 def cnf(tree):
     return dist(rneg(rimp(tree)))
+
+# Tests
+# rimp tests
+# rneg tests
+# dist tests
+
