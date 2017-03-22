f03fd77b126e38cd356a10a1181962433de3d644
tauomicronmu <tauomicronmurs@gmail.com>
2017-03-22 00:57:14 +0000

    added flag to the executable

--- cmdln.py
+++ cmdln.py
@@ -7,9 +7,17 @@ import res
 args = sys.argv
 
 def satcheck():
-    if len(args) != 2:
+    if len(args) != 3:
         print("Wrong arg length! Usage: python3 cmdln <expression>")
         return
+    flag = args[1]
+    expr = args[2]
+    if flag == "cnf":
+        return cnf.clause_nf(expr)
+    if flag == "res": 
+        return res.res(args[1])
+    if flag == "dpll":
+        pass
     res.res(args[1])
 
 satcheck()
